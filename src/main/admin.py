c6d687f4878fef1b1dc9b71feaea7bba1dd0a7e3
Andrii Demchenko <andriidem308.job@gmail.com>
2025-05-22 13:17:30 +0300

    tasks/task-2 task 2 done

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Contact, CV, Project, Skill
 
-# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(CV)
+admin.site.register(Project)
+admin.site.register(Skill)
